908d97e77c9f61b09dd6f555ec848a5b9393bff7
99tut <3530445089@qq.com>
2023-03-09 17:33:24 +0800

    初始DDD框架搭建

---
+++ chatbot-api-interfaces/src/main/java/cn/tut/chatbot/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package cn.tut.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: ApiApplication
+ * @Description: 启动入口
+ * @Author: tut
+ **/
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/tut/chatbot/api/test/ApiTest.java
@@ -0,0 +1,10 @@
+package cn.tut.chatbot.api.test;
+
+/**
+ * @ClassName: ApiTest
+ * @Description: ≤‚ ‘
+ * @Author: tut
+ **/
+public class ApiTest {
+
+}
